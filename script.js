8a1e6e159497f351099b47d6056bc3015094abdc
nany9 <56671047+nany9@users.noreply.github.com>
2024-08-30 10:41:40 +0300

    Update script.js
    added haptic feedback

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ const tg = window.Telegram.WebApp;
 tg.showScanQrPopup({text: "Отсканируйте QR-код на чеке"}, function(text){
     if (text.slice(0,5) == 't=202'){
         tg.sendData(text);
+        tg.notificationOccurred("success");
         tg.closeScanQrPopup();
     } else {
         tg.showAlert('Неверный QR код');
+        tg.notificationOccurred("error");
     }
  
 });
