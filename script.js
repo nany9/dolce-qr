a96f38cbe01e9869077a6076a41e37abe5970a57
nany9 <msx009@mail.ru>
2024-08-30 07:46:49 +0300

    second commit

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const tg = window.Telegram.WebApp;
 tg.showScanQrPopup({text: "Отсканируйте QR-код на чеке"}, function(text){
-    tg.sendData(text);
-    tg.closeScanQrPopup();
+    if (text.slice(0,5) == 't=202'){
+        tg.sendData(text);
+        tg.closeScanQrPopup();
+    } else {
+        tg.showAlert('Неверный QR код');
+    }
+ 
 });
 
 tg.onEvent("scanQrPopupClosed", function(){
